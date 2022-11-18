906a2a4a87fa7d832d01eaf87c28aad41910e57a
zezerinodev <mattoszz@hotmail.com>
2022-11-17 22:03:21 -0300

    Ícones dos cartões carregando dinamicamente.

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -4,13 +4,15 @@ import { format } from 'date-fns';
 
 //MY IMPORTS
 import styles from './styles';
-import iconDefault from '../../assets/default.png';
 
-export default function TaskCard({done, title, when}){
+// COLECAO DE ICONES
+import typeIcons from '../../utils/typeIcons';
+
+export default function TaskCard({done, title, when, type}){
     return(
         <TouchableOpacity style={[styles.card, done && styles.done]}>
             <View style={styles.cardLeft}>
-                <Image source={iconDefault} style={styles.typeActive}/>
+                <Image source={typeIcons[type]} style={styles.typeActive}/>
                 <Text style={styles.cardTitle}>{title}</Text>
             </View>
 
